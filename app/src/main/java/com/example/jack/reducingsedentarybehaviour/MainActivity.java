bd971af583bc7a484938eb89ad0263e3111d3895
Jack-Dempster <jackdempster17@gmail.com>
2017-02-16 17:26:51 +0000

    Initial commit

---
+++ app/src/main/java/com/example/jack/reducingsedentarybehaviour/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.jack.reducingsedentarybehaviour;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.result.DailyTotalResult;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+
+    private Button mButtonViewWeek;
+    private Button mButtonViewToday;
+    private static final String TAG = "MainActivity";
+
+    private GoogleApiClient mGoogleApiClient;
+    private long stepTotal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //mButtonViewWeek = (Button) findViewById(R.id.btn_view_week);
+        mButtonViewToday = (Button) findViewById(R.id.btn_view_today);
+       // mButtonViewWeek.setOnClickListener(this);
+        mButtonViewToday.setOnClickListener(this);
+
+        //Connect to googleApi
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, 0, this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    private class ViewDailyStepCountTask extends AsyncTask<Void, Void, Void> {
+        protected Void doInBackground(Void... params) {
+            long total = 0;
+
+            PendingResult<DailyTotalResult> result = Fitness.HistoryApi.readDailyTotal(mGoogleApiClient, DataType.TYPE_STEP_COUNT_DELTA);
+            DailyTotalResult totalResult = result.await(30, TimeUnit.SECONDS);
+            if (totalResult.getStatus().isSuccess()) {
+                DataSet totalSet = totalResult.getTotal();
+                total = totalSet.isEmpty()
+                        ? 0
+                        : totalSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+            } else {
+                Log.w(TAG, "There was a problem getting the step count.");
+            }
+            stepTotal = total;
+            Log.i(TAG, "Total steps: " + total);
+
+            return null;
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("HistoryAPI", "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("HistoryAPI", "onConnectionFailed");
+    }
+
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.e("HistoryAPI", "onConnected");
+    }
+
+
+    @Override
+    public void onClick(View v) {
+       switch (v.getId()){
+           case R.id.btn_view_week:
+               //new ViewWeekStepCountTask().execute();
+               break;
+           case R.id.btn_view_today:
+               showFilterPopup(v);
+               new ViewDailyStepCountTask().execute();
+                       break;
+       }
+    }
+
+    // Display anchored popup menu based on view selected
+    private void showFilterPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        // Inflate the menu from xml
+        popup.getMenuInflater().inflate(R.menu.popup_filters, popup.getMenu());
+        // Setup menu item selection
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_keyword:
+                        Toast.makeText(MainActivity.this, "Random Text", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menu_popularity:
+                        Toast.makeText(MainActivity.this, "Steps:" + stepTotal, Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        // Handle dismissal with: popup.setOnDismissListener(...);
+        popup.setOnDismissListener(null);
+        // Show the menu
+        popup.show();
+    }
+}
