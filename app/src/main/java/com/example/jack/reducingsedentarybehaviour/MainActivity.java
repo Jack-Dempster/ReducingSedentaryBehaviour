f6501f97c9013d8c6b8a6e2230a868c340a7e773
Jack-Dempster <jackdempster17@gmail.com>
2017-03-05 19:30:33 +0000

    Reverted to previous design without navigation drawer. Added methods to retrieve weekly data and represent the data in graphs.

--- app/src/main/java/com/example/jack/reducingsedentarybehaviour/MainActivity.java
+++ app/src/main/java/com/example/jack/reducingsedentarybehaviour/MainActivity.java
@@ -6,14 +6,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -34,6 +31,7 @@ import com.txusballesteros.widgets.FitChart;
 import com.txusballesteros.widgets.FitChartValue;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
@@ -57,16 +55,9 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        buildFitClient();
+        getSupportActionBar().setTitle(new SimpleDateFormat("EEEE dd MMMM").format(new Date()));//Set day and date as the title
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        buildFitClient();
 
     }
 
